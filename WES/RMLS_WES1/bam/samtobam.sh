65df8e5ef290d36b225b6b36889997a3ca60c8c9
jee9 <jeeh9912@gmail.com>
2025-08-06 14:49:19 +0900

    Create samtobam.sh

---
+++ WES/RMLS_WES1/bam/samtobam.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+samtools view -Sb -@ 10 /HDD8T3/jeeh99/RMLS_WES1/bwa/MLS_1.sam > /HDD8T3/jeeh99/RMLS_WES1/bam/MLS_1.bam
+
+samtools view -Sb -@ 10 /HDD8T3/jeeh99/RMLS_WES1/bwa/MLS_2.sam > /HDD8T3/jeeh99/RMLS_WES1/bam/MLS_2.bam
+
+samtools view -Sb -@ 10 /HDD8T3/jeeh99/RMLS_WES1/bwa/MLS_3.sam > /HDD8T3/jeeh99/RMLS_WES1/bam/MLS_3.bam
+
+samtools view -Sb -@ 10 /HDD8T3/jeeh99/RMLS_WES1/bwa/MLS_4.sam > /HDD8T3/jeeh99/RMLS_WES1/bam/MLS_4.bam
+
+samtools view -Sb -@ 10 /HDD8T3/jeeh99/RMLS_WES1/bwa/MLS_5.sam > /HDD8T3/jeeh99/RMLS_WES1/bam/MLS_5.bam
+
+samtools view -Sb -@ 10 /HDD8T3/jeeh99/RMLS_WES1/bwa/NL_1.sam > /HDD8T3/jeeh99/RMLS_WES1/bam/NL_1.bam
+
+samtools view -Sb -@ 10 /HDD8T3/jeeh99/RMLS_WES1/bwa/NL_2.sam > /HDD8T3/jeeh99/RMLS_WES1/bam/NL_2.bam
+
+samtools view -Sb -@ 10 /HDD8T3/jeeh99/RMLS_WES1/bwa/NL_3.sam > /HDD8T3/jeeh99/RMLS_WES1/bam/NL_3.bam
+
+samtools view -Sb -@ 10 /HDD8T3/jeeh99/RMLS_WES1/bwa/NL_4.sam > /HDD8T3/jeeh99/RMLS_WES1/bam/NL_4.bam
+
+samtools view -Sb -@ 10 /HDD8T3/jeeh99/RMLS_WES1/bwa/NL_5.sam > /HDD8T3/jeeh99/RMLS_WES1/bam/NL_5.bam
+
+samtools view -Sb -@ 10 /HDD8T3/jeeh99/RMLS_WES1/bwa/RMLS_1.sam > /HDD8T3/jeeh99/RMLS_WES1/bam/RMLS_1.bam
+
+samtools view -Sb -@ 10 /HDD8T3/jeeh99/RMLS_WES1/bwa/RMLS_2.sam > /HDD8T3/jeeh99/RMLS_WES1/bam/RMLS_2.bam
+
+samtools view -Sb -@ 10 /HDD8T3/jeeh99/RMLS_WES1/bwa/RMLS_3.sam > /HDD8T3/jeeh99/RMLS_WES1/bam/RMLS_3.bam
+
+samtools view -Sb -@ 10 /HDD8T3/jeeh99/RMLS_WES1/bwa/RMLS_4.sam > /HDD8T3/jeeh99/RMLS_WES1/bam/RMLS_4.bam
+
+samtools view -Sb -@ 10 /HDD8T3/jeeh99/RMLS_WES1/bwa/RMLS_5.sam > /HDD8T3/jeeh99/RMLS_WES1/bam/RMLS_5.bam
