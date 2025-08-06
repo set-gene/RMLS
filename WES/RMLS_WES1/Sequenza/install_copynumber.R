d00448e10431364182edc374d92a9f73d74e00d1
jee9 <jeeh9912@gmail.com>
2025-08-06 17:30:44 +0900

    Create install_copynumber.R

---
+++ WES/RMLS_WES1/Sequenza/install_copynumber.R
@@ -0,0 +1,9 @@
+ #!/usr/bin/env Rscript
+
+
+# You can install the orignal package from bioconductor fistly
+# to install all dependencies
+# BiocManager::install("copynumber")
+
+# Then install this hg38-supported package
+remotes::install_github("ShixiangWang/copynumber")
