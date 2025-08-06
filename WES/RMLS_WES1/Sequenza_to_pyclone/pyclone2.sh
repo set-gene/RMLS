1cf8a05791dc67ef6943cc9b21cb3dc94098e959
jee9 <jeeh9912@gmail.com>
2025-08-06 17:59:33 +0900

    Create pyclone2.sh

---
+++ WES/RMLS_WES1/Sequenza_to_pyclone/pyclone2.sh
@@ -0,0 +1,18 @@
+PyClone run_analysis_pipeline --in_files MLS_1.tsv RMLS_1.tsv --tumour_contents 0.82 0.76 --min_cluster_size 2 --prior major_copy_number --density pyclone_binomial --working_dir ./Pyclone/Sample_1_pyclone_analysis --seed 999 >> ./Pyclone/sample1.log 2>&1
+
+PyClone run_analysis_pipeline --in_files MLS_2.tsv RMLS_2.tsv --tumour_contents 0.97 0.99 --min_cluster_size 2 --max_clusters 10 --prior major_copy_number --density pyclone_binomial --working_dir ./Pyclone/Sample_2_pyclone_analysis --seed 999 >> ./Pyclone/sample2.log 2>&1
+
+PyClone run_analysis_pipeline --in_files MLS_3.tsv RMLS_3.tsv --tumour_contents 0.90 0.68 --min_cluster_size 2 --prior major_copy_number --density pyclone_binomial --working_dir ./Pyclone/Sample_3_pyclone_analysis --seed 999 >> ./Pyclone/sample3.log 2>&1
+
+PyClone run_analysis_pipeline --in_files MLS_4.tsv RMLS_4.tsv --tumour_contents 0.57 0.77 --min_cluster_size 2 --prior major_copy_number --density pyclone_binomial --working_dir ./Pyclone/Sample_4_pyclone_analysis --seed 999 >> ./Pyclone/sample4.log 2>&1 
+
+PyClone run_analysis_pipeline --in_files MLS_5.tsv RMLS_5.tsv --tumour_contents 0.86 0.42 --min_cluster_size 2 --prior major_copy_number --density pyclone_binomial --working_dir ./Pyclone/Sample_5_pyclone_analysis --seed 999 >> ./Pyclone/sample5.log 2>&1
+
+PyClone run_analysis_pipeline --in_files MLS_6.tsv RMLS_6.tsv --tumour_contents 0.37 0.68 --min_cluster_size 2 --prior major_copy_number --density pyclone_binomial --working_dir ./Pyclone/Sample_6_pyclone_analysis --seed 999 >> ./Pyclone/sample6.log 2>&1
+
+PyClone run_analysis_pipeline --in_files MLS_7.tsv RMLS_7.tsv --tumour_contents 0.42 0.40 --min_cluster_size 2 --prior major_copy_number --density pyclone_binomial --working_dir ./Pyclone/Sample_7_pyclone_analysis --seed 999 >> ./Pyclone/sample7.log 2>&1
+
+PyClone run_analysis_pipeline --in_files MLS_8.tsv RMLS_8.tsv --tumour_contents 0.90 0.90 --min_cluster_size 2 --prior major_copy_number --density pyclone_binomial --working_dir ./Pyclone/Sample_8_pyclone_analysis --seed 999 >> ./Pyclone/sample8.log 2>&1
+
+PyClone run_analysis_pipeline --in_files MLS_9.tsv RMLS_9.tsv --tumour_contents 0.37 0.39 --min_cluster_size 2 --prior major_copy_number --density pyclone_binomial --working_dir ./Pyclone/Sample_9_pyclone_analysis --seed 999 >> ./Pyclone/sample9.log 2>&1 
+
