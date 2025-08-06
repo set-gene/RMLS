979b80b1a143f370f4f03db3ad53833e7e9bd7a9
jee9 <jeeh9912@gmail.com>
2025-08-06 17:28:01 +0900

    Create sequenza_final.sh

---
+++ WES/RMLS_WES1/Sequenza/sequenza_final.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+Rscript run_sequenza.R  -i MLS_1.small.seqz.gz -n MLS_1 -o ./res
+
+Rscript run_sequenza.R  -i MLS_2.small.seqz.gz -n MLS_2 -o ./res
+
+Rscript run_sequenza.R  -i MLS_3.small.seqz.gz -n MLS_3 -o ./res
+
+Rscript run_sequenza.R  -i MLS_4.small.seqz.gz -n MLS_4 -o ./res
+
+Rscript run_sequenza.R  -i MLS_5.small.seqz.gz -n MLS_5 -o ./res
+
+Rscript run_sequenza.R  -i RMLS_1.small.seqz.gz -n RMLS_1 -o ./res
+
+Rscript run_sequenza.R  -i RMLS_2.small.seqz.gz -n RMLS_2 -o ./res
+
+Rscript run_sequenza.R  -i RMLS_3.small.seqz.gz -n RMLS_3 -o ./res
+
+Rscript run_sequenza.R  -i RMLS_4.small.seqz.gz -n RMLS_4 -o ./res
+
+Rscript run_sequenza.R  -i RMLS_5.small.seqz.gz -n RMLS_5 -o ./res
