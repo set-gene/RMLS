25c6c3f2dfb8e6ca875839bf941158885f40881e
jee9 <jeeh9912@gmail.com>
2025-08-06 14:51:25 +0900

    Create bamsort.sh

---
+++ WES/RMLS_WES1/sort/bamsort.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+samtools sort -@ 10 -o /HDD8T3/jeeh99/RMLS_WES1/sort/NL_1_sort.bam /HDD8T3/jeeh99/RMLS_WES1/bam/NL_1.bam
+
+samtools sort -@ 10 -o /HDD8T3/jeeh99/RMLS_WES1/sort/NL_2_sort.bam /HDD8T3/jeeh99/RMLS_WES1/bam/NL_2.bam
+
+samtools sort -@ 10 -o /HDD8T3/jeeh99/RMLS_WES1/sort/NL_3_sort.bam /HDD8T3/jeeh99/RMLS_WES1/bam/NL_3.bam
+
+samtools sort -@ 10 -o /HDD8T3/jeeh99/RMLS_WES1/sort/NL_4_sort.bam /HDD8T3/jeeh99/RMLS_WES1/bam/NL_4.bam
+
+samtools sort -@ 10 -o /HDD8T3/jeeh99/RMLS_WES1/sort/NL_5_sort.bam /HDD8T3/jeeh99/RMLS_WES1/bam/NL_5.bam
+
+samtools sort -@ 10 -o /HDD8T3/jeeh99/RMLS_WES1/sort/MLS_1_sort.bam /HDD8T3/jeeh99/RMLS_WES1/bam/MLS_1.bam
+
+samtools sort -@ 10 -o /HDD8T3/jeeh99/RMLS_WES1/sort/MLS_2_sort.bam /HDD8T3/jeeh99/RMLS_WES1/bam/MLS_2.bam
+
+samtools sort -@ 10 -o /HDD8T3/jeeh99/RMLS_WES1/sort/MLS_3_sort.bam /HDD8T3/jeeh99/RMLS_WES1/bam/MLS_3.bam
+
+samtools sort -@ 10 -o /HDD8T3/jeeh99/RMLS_WES1/sort/MLS_4_sort.bam /HDD8T3/jeeh99/RMLS_WES1/bam/MLS_4.bam
+
+samtools sort -@ 10 -o /HDD8T3/jeeh99/RMLS_WES1/sort/MLS_5_sort.bam /HDD8T3/jeeh99/RMLS_WES1/bam/MLS_5.bam
+
+samtools sort -@ 10 -o /HDD8T3/jeeh99/RMLS_WES1/sort/RMLS_1_sort.bam /HDD8T3/jeeh99/RMLS_WES1/bam/RMLS_1.bam
+
+samtools sort -@ 10 -o /HDD8T3/jeeh99/RMLS_WES1/sort/RMLS_2_sort.bam /HDD8T3/jeeh99/RMLS_WES1/bam/RMLS_2.bam
+
+samtools sort -@ 10 -o /HDD8T3/jeeh99/RMLS_WES1/sort/RMLS_3_sort.bam /HDD8T3/jeeh99/RMLS_WES1/bam/RMLS_3.bam
+
+samtools sort -@ 10 -o /HDD8T3/jeeh99/RMLS_WES1/sort/RMLS_4_sort.bam /HDD8T3/jeeh99/RMLS_WES1/bam/RMLS_4.bam
+
+samtools sort -@ 10 -o /HDD8T3/jeeh99/RMLS_WES1/sort/RMLS_5_sort.bam /HDD8T3/jeeh99/RMLS_WES1/bam/RMLS_5.bam
+
