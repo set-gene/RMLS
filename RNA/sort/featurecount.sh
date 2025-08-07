4499bdaab5d0b8951cdc5ccea9f644455ce5e85d
jee9 <jeeh9912@gmail.com>
2025-08-07 13:00:43 +0900

    Create featurecount.sh

---
+++ RNA/sort/featurecount.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+cat /HDD8T/jeeh99/0114_RMLS_RNA/sort/sample.txt | while read id
+do
+ echo "start featureCounts for ${id}" 'date'
+ featureCounts -T 6 -p -t exon -g gene_name -a /HDD2T/jeeh9/Reference/RNA_reference/gencode.v35.annotation.gtf -o /HDD8T/jeeh99/0114_RMLS_RNA/DEG/gene_featurecounts/${id}.txt /HDD8T/jeeh99/0114_RMLS_RNA/sort/${id}_sort.bam 2>&1|tee /HDD8T/jeeh99/0114_RMLS_RNA/DEG/gene_featurecounts/${id}_featurecounts.log
+ echo "end featureCounts for ${id}" 'date'
+done
