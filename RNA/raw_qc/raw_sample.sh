71eb297b9122a0910b6cef218dc882aa8e917ec6
jee9 <jeeh9912@gmail.com>
2025-08-07 11:10:36 +0900

    Create raw_sample.sh

---
+++ RNA/raw_qc/raw_sample.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+#RNA_raw_data
+
+wget -c https://data.macrogen.com/~macro3/HiSeq02//20210112/HN00137932/MLS1_1.fastq.gz
+wget -c https://data.macrogen.com/~macro3/HiSeq02//20210112/HN00137932/MLS1_2.fastq.gz
+wget -c https://data.macrogen.com/~macro3/HiSeq02//20210112/HN00137932/MLS3_1.fastq.gz
+wget -c https://data.macrogen.com/~macro3/HiSeq02//20210112/HN00137932/MLS3_2.fastq.gz
+wget -c https://data.macrogen.com/~macro3/HiSeq02//20210112/HN00137932/MLS4_1.fastq.gz
+wget -c https://data.macrogen.com/~macro3/HiSeq02//20210112/HN00137932/MLS4_2.fastq.gz
+wget -c https://data.macrogen.com/~macro3/HiSeq02//20210112/HN00137932/MLS5_1.fastq.gz
+wget -c https://data.macrogen.com/~macro3/HiSeq02//20210112/HN00137932/MLS5_2.fastq.gz
+wget -c https://data.macrogen.com/~macro3/HiSeq02//20210112/HN00137932/NL3_1.fastq.gz
+wget -c https://data.macrogen.com/~macro3/HiSeq02//20210112/HN00137932/NL3_2.fastq.gz
+wget -c https://data.macrogen.com/~macro3/HiSeq02//20210112/HN00137932/NL4_1.fastq.gz
+wget -c https://data.macrogen.com/~macro3/HiSeq02//20210112/HN00137932/NL4_2.fastq.gz
+wget -c https://data.macrogen.com/~macro3/HiSeq02//20210112/HN00137932/NL5_1.fastq.gz
+wget -c https://data.macrogen.com/~macro3/HiSeq02//20210112/HN00137932/NL5_2.fastq.gz
+wget -c https://data.macrogen.com/~macro3/HiSeq02//20210112/HN00137932/RMLS1_1.fastq.gz
+wget -c https://data.macrogen.com/~macro3/HiSeq02//20210112/HN00137932/RMLS1_2.fastq.gz
+wget -c https://data.macrogen.com/~macro3/HiSeq02//20210112/HN00137932/RMLS2_1.fastq.gz
+wget -c https://data.macrogen.com/~macro3/HiSeq02//20210112/HN00137932/RMLS2_2.fastq.gz
+wget -c https://data.macrogen.com/~macro3/HiSeq02//20210112/HN00137932/RMLS3_1.fastq.gz
+wget -c https://data.macrogen.com/~macro3/HiSeq02//20210112/HN00137932/RMLS3_2.fastq.gz
+wget -c https://data.macrogen.com/~macro3/HiSeq02//20210112/HN00137932/RMLS4_1.fastq.gz
+wget -c https://data.macrogen.com/~macro3/HiSeq02//20210112/HN00137932/RMLS4_2.fastq.gz
+wget -c https://data.macrogen.com/~macro3/HiSeq02//20210112/HN00137932/RMLS5_1.fastq.gz
+wget -c https://data.macrogen.com/~macro3/HiSeq02//20210112/HN00137932/RMLS5_2.fastq.gz
